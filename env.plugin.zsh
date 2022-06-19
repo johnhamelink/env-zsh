10313d34a4a03d5fb53de9f770e8768e98b77624
ea-sam <65998880+ea-sam@users.noreply.github.com>
2022-06-19 17:09:35 +0530

    Update env.plugin.zsh

--- env.plugin.zsh
+++ env.plugin.zsh
@@ -4,7 +4,7 @@ autoload -U add-zsh-hook
 load-local-conf() {
      # check file exists, is regular file and is readable:
      if [[ -f .env && -r .env ]]; then
-       source .env
+       export $(grep -v '^#' .env | xargs)
      fi
 }
 
